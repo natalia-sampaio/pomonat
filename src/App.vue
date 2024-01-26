240561ab951f870d289a272d34c129b008551909
Natalia Andrade <natalia.srsa@proton.me>
2024-01-26 18:17:41 -0300

    :construction: wip: adding settings modal

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,23 @@ import IconSettings from './components/icons/IconSettings.vue';
     <img alt="pomodoro logo" class="" src="@/assets/logo.svg" width="125" height="125" />
     <TimerIndicator />
     <TimerButton />
-    <IconSettings />
+    
+    <!-- You can open the modal using ID.showModal() method -->
+    <button class="" onclick="my_modal_3.showModal()"><IconSettings /></button>
+    <dialog id="my_modal_3" class="modal">
+      <div class="modal-box">
+        <form method="dialog">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="font-bold text-lg">Hello!</h3>
+        <p class="py-4">Press ESC key, click on ✕ button or click on close button to close</p>
+        <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn bg-primary">Apply</button>
+        </form>
+      </div>
+      </div>
+    </dialog>
   </div>
 </template>
