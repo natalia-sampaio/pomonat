1641b0a5365791a04da41fd72b6a77d838b6d6bd
Natalia Andrade <natalia.srsa@proton.me>
2024-01-26 02:20:37 -0300

    :heavy_plus_sign: build: added theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        'red': 'F87070',
+        'blue': '70F3F8',
+        'purple': 'D881F8',
+        'background': '1E213F',
+        'surface': '161932',
+        'modal-bg': 'FFFFFF',
+        'modal-surface': 'EFF1FA',
+        'text': 'D7E0FF',
+      },
+      fontFamily: {
+        kumbh: ['Kumbh Sans', 'sans-serif'],
+        roboto: ['Roboto Slab', 'serif'],
+        space: ['Space Mono', 'monospace']
+      }
+    },
   },
   plugins: [],
 }
